35f9bdd4ca4028a2abb0bdfaf95e8728e96b4357
cristian-sz <cristian.szatmari@lateralgroup.com>
2024-10-25 18:11:55 +0300

    added cors and changed backend port to 8080

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,16 @@
 const express = require('express');
+const cors = require('cors');
+
 const app = express();
-const PORT = 5000;
 
-app.use(express.json());
+app.use(cors({
+    origin: 'http://localhost:3000'
+}));
 
-app.get('/api', (req, res) => {
+app.get('/', (req, res) => {
     res.json({ message: 'Hello from the backend!' });
 });
 
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+app.listen(8080, () => {
+    console.log('Server running on http://localhost:8080');
 });
